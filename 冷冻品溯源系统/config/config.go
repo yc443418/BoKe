61b0db543dfeaac3c7730a2821d321b86fb48917
yc443418 <yc3249673079@163.com>
2025-05-16 10:53:20 +0800

    Delete 冷冻品溯源系统 directory

--- 冷冻品溯源系统/config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-type Config struct {
-	MySQL   Mysql
-	Redis   Redis
-	Jwt     Jwt
-	Loggger Loggger
-	Server  Server
-}
--- 冷冻品溯源系统/config/jwt.go
+++
@@ -1,9 +0,0 @@
-package config
-
-// JWT 配置结构体
-type Jwt struct {
-	Issuer     string `json:"issuer"`
-	Secret     string `json:"secret"`
-	ExpireTime int64  `json:"expireTime"`
-	NotBefore  int64  `json:"notBefore"`
-}
